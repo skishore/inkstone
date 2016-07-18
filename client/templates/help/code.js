b6baa9610135865e6302945c098aa0330e9c4340
Shaunak Kishore <kshaunak@gmail.com>
2016-07-18 11:27:34 -0400

    Get basic guides working

--- client/templates/help/code.js
+++ client/templates/help/code.js
@@ -1,18 +1,39 @@
-const params = new ReactiveVar();
+import {Backdrop} from '/client/backdrop';
+import {Overlay} from '/client/templates/overlay/code';
 
-Template.demo.helpers({
-  params() {
-    return params.get();
-  },
-  transform() {
-    return params.get() ? 'translateY(0)' : undefined;
-  },
-});
+const kSelectors = {
+  add_custom_word_lists: '.lists',
+  practice_writing: '.teach',
+  turn_off_snap_strokes: '.settings',
+  tweak_scheduling: '.settings',
+};
+
+const params = new ReactiveDict();
+
+Template.demo.helpers({get: (key) => params.get(key)});
 
 Template.help.events({
   'click .item.help-item': function(event) {
-    console.log(this.help);
+    params.clear();
+    params.set('topic', this.topic);
+    Backdrop.show();
   },
 });
 
-window.params = params;
+Meteor.startup(() => {
+  const index = window.location.search.indexOf('demo=');
+  if (index < 0) return;
+  Template.layout.onRendered(() => {
+    parent.postMessage('Demo iframe loaded.', '*');
+    const topic = window.location.search.substr(index + 5);
+    const selector = kSelectors[topic];
+    Meteor.setTimeout((() => Overlay.show($(selector))), 600);
+  });
+});
+
+window.addEventListener('message', (event) => {
+  if (event.data === 'Demo iframe loaded.') {
+    params.set('transform', 'translateY(0)');
+    Backdrop.hide();
+  }
+});
