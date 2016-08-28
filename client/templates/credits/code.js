65ed3f9f9d1276fd2b27c746c1878559ccc89be0
Shaunak Kishore <kshaunak@gmail.com>
2016-08-28 04:42:05 -0400

    Get links on credits page working

---
+++ client/templates/credits/code.js
@@ -0,0 +1,27 @@
+/*
+ *  Copyright 2016 Shaunak Kishore (kshaunak "at" gmail.com)
+ *
+ *  This file is part of Inkstone.
+ *
+ *  Inkstone is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Inkstone is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Inkstone.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+Template.credits.events({
+  'click .item.credits-item': function(event) {
+    const element = $(event.currentTarget);
+    const link = element.parent().find('.item.credits-item.link');
+    if (link.length === 0) return;
+    window.open(link.text().trim(), '_system');
+  },
+});
