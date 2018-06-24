65a5f502c18470488a4661b90765f4fb824f18aa
Shaunak Kishore <kshaunak@gmail.com>
2018-06-24 18:22:46 -0400

    Bump version number to 0.1.4

--- mobile-config.js
+++ mobile-config.js
@@ -2,6 +2,6 @@ App.accessRule('https://www.skishore.me/inkstone/*');
 App.info({
   name: 'Inkstone',
   description: 'Learn to write Chinese characters.',
-  version: '0.1.3'
+  version: '0.1.4'
 });
 App.setPreference('orientation', 'portrait');
